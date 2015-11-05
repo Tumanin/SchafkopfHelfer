1003b60f47e7468c379227f58c16fe4bd74f609e
Tumanin <atumanin@appmagnetics.de>
2015-11-05 12:23:35 +0100

    Initial commit

---
+++ app/src/main/java/com/applicatum/schafkopfhelfer/MainActivity.java
@@ -0,0 +1,76 @@
+package com.applicatum.schafkopfhelfer;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.applicatum.schafkopfhelfer.fragments.StartFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        */
+        StartFragment fragment = new StartFragment();
+        startFragment(fragment, false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void startFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+
+        if(addToBackStack) {
+            fragmentTransaction.addToBackStack("frame");
+        }
+        else {
+            fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        }
+
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/applicatum/schafkopfhelfer/adapters/UsersDynamicAdapter.java
@@ -0,0 +1,80 @@
+package com.applicatum.schafkopfhelfer.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.applicatum.schafkopfhelfer.R;
+import com.applicatum.schafkopfhelfer.models.User;
+
+import org.askerov.dynamicgrid.BaseDynamicGridAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Alexx on 02.11.2015.
+ */
+public class UsersDynamicAdapter extends BaseDynamicGridAdapter {
+
+    //private List<User> users;
+    //private Context context;
+    private LayoutInflater mInflater;
+
+    public UsersDynamicAdapter(Context context, List<User> items, int columnCount) {
+
+        super(context, items, columnCount);
+        //users = items;
+        //this.context = context;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        UserViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.user_item_layout, null);
+            holder = new UserViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (UserViewHolder) convertView.getTag();
+        }
+
+        User user = (User) getItem(position);
+        holder.build(user.getName(), user.getPoints(), getState(user));
+        return convertView;
+    }
+
+    private String getState(User user){
+        switch (user.getState()){
+            case PLAY:
+                return "spielt";
+            case WAIT:
+                return "wartet";
+            case WIN:
+                return "gewonnen";
+        }
+        return "";
+    }
+
+    private class UserViewHolder {
+        private TextView txtName;
+        private TextView txtPoints;
+        private TextView txtState;
+
+        private UserViewHolder(View view) {
+            txtName = (TextView) view.findViewById(R.id.userName);
+            txtPoints = (TextView) view.findViewById(R.id.userPoints);
+            txtState = (TextView) view.findViewById(R.id.userState);
+        }
+
+        void build(String name, int points, String state) {
+            txtName.setText(name);
+            txtPoints.setText(String.valueOf(points));
+            txtState.setText(state);
+        }
+    }
+
+}
