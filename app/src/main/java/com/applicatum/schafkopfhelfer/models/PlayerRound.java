21fc0f12a85a7c36611b41c8631d3c0b2eab3fc3
Dominik Serve <serve@fs.tum.de>
2015-11-30 20:05:20 +0100

    recordNewRound added

--- app/src/main/java/com/applicatum/schafkopfhelfer/models/PlayerRound.java
+++ app/src/main/java/com/applicatum/schafkopfhelfer/models/PlayerRound.java
@@ -13,18 +13,25 @@ public class PlayerRound extends SugarRecord<PlayerRound> {
     Player player;
     Round round;
     boolean gewonnen;
-    int punkte;
+    int globalPoints;
+    int gamePoints;
+    int changePoints;
+    boolean jungfrau;
 
     public PlayerRound(){
 
     }
 
-    public PlayerRound(Player player, Round round, boolean gewonnen, int punkte){
+    public PlayerRound(Player player, Round round, boolean gewonnen, int changePoints, boolean jungfrau){
         PlayerRound pr = new PlayerRound();
         pr.player = player;
+        player.addPoints(changePoints);
         pr.round = round;
         pr.gewonnen = gewonnen;
-        pr.punkte = punkte;
+        pr.globalPoints = player.getGlobalPoints();
+        pr.gamePoints = player.getGamePoints();
+        pr.changePoints = changePoints;
+        pr.jungfrau = jungfrau;
         pr.save();
     }
 
@@ -32,7 +39,7 @@ public class PlayerRound extends SugarRecord<PlayerRound> {
         return PlayerRound.listAll(PlayerRound.class);
     }
 
-    public static void deletePlayerRound(Round r){
+    public static void deletePlayerRound(Round r) {
         PlayerRound.deleteAll(PlayerRound.class, "round = ?", String.valueOf(r.getId()));
     }
 
